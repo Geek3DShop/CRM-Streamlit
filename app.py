fcb7edba858244d806c0f2e703ed502449d8bf26
Fernando <fernandocr019@gmail.com>
2026-08-06 17:42:04 -0300

    update no visual da tabela

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+st.set_page_config(page_title="CRM Geek 3D Shop", layout="wide")
+
 st.title("CRM Geek 3D Shop")
 
 path = os.getenv("CRM_PATH")
 df = pd.read_excel(path)
-st.dataframe(df)
+df = df.dropna(axis=1, how="all")
+
+st.dataframe(df, width="stretch")
